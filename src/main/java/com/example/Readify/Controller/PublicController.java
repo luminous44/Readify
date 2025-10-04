5d79322fca113d2137cac9ffce4531cfc7e22c4b
luminous44 <aklemasultanakhi@gmail.com>
2025-10-04 20:30:14 +0600

    initial commit

---
+++ src/main/java/com/example/Readify/Controller/PublicController.java
@@ -0,0 +1,16 @@
+package com.example.Readify.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PublicController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "Ok";
+    }
+}
---
+++ src/test/java/com/example/Readify/ReadifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Readify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
